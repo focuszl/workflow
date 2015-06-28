4e449d6663e00001f913f9b77f831e1e81b33057
focuszl <ihailin@qq.com>
2015-06-28 15:49:52 +0800

    安装钩子

--- src/main/java/org/focuszl/workflow/FuntionTest.java
+++ src/main/java/org/focuszl/workflow/FuntionTest.java
@@ -1,14 +1,16 @@
 package org.focuszl.workflow;
 
+
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 
 
@@ -20,28 +22,65 @@ public class FuntionTest {
 //		datas = new ArrayList<Data>();
 		datas.add(new Data());
 		completionService = new ExecutorCompletionService<Boolean>(Executors.newCachedThreadPool());
+		Runtime.getRuntime().addShutdownHook(getSystemShutdownHook());
+//		try {
+//			Runtime.getRuntime().exec("");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	public static void main(String[] args) {
 		FuntionTest funtionTest = new FuntionTest();
-		funtionTest.test();	
+		boolean success = funtionTest.test();
+		if(success){
+			System.out.println("child success");
+			System.exit(0);
+		}
+		
 		
 	}
+	private class SystemShutdownHook extends Thread{
+		
+		public SystemShutdownHook(){
+			super();		
+			
+		}
+		
+		@Override
+		public void run(){
+			System.out.println("exit");//虚拟机结束或者调用system.exit(0)时调用			
+		}
+	};
+	
+	public SystemShutdownHook getSystemShutdownHook(){
+		return new SystemShutdownHook();
+	}
 
-	public void test(){
+	public boolean test(){
 		//创建多个线程，将datas的copy传给它们，看它们是否能保证执行顺序
 		final 
 		Callable<Boolean> callable = new Callable<Boolean>() {			
-			public Boolean call() throws InterruptedException{
+			public Boolean call() throws InterruptedException {
 				final List<Data> copy = new ArrayList<Data>(datas);
 				for(int i = 0; i < 3; i++){
 					System.out.println("begin cycle");
-					ExecutorService excutor = Executors.newSingleThreadExecutor();
+					ExecutorService excutor = Executors.newSingleThreadExecutor(new ThreadFactory() {
+						private final ThreadFactory factory = Executors.defaultThreadFactory();
+						@Override
+						public Thread newThread(Runnable r) {
+							Thread thread = factory.newThread(r);
+							thread.setDaemon(true);
+							return thread;
+						}
+					});
+//					ExecutorService excutor = Executors.newFixedThreadPool(3);
 					Callable<Boolean> task = new Callable<Boolean>() {
 						 public Boolean call(){
 							try {
 								System.out.println(Thread.currentThread().getName() + " begin");
 								copy.get(0).name = Thread.currentThread().getName();
-								Thread.sleep(3000);
+								Thread.sleep(1000);
 								System.out.println(copy.get(0).name + " end");
 								System.out.println();
 							} catch (Exception e) {
@@ -59,6 +98,19 @@ public class FuntionTest {
 			}
 		};
 		completionService.submit(callable);
+		try {
+			if(completionService.take().get()){
+				return true;
+			}
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 
